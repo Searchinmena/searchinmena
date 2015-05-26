f00150ca690846f0958dcfbea5ccbcdef66f3f28
Malgorzata <malgorzata@lunarlogic.io>
2015-05-26 14:45:36 +0200

    dashboard view first design draft

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
   get "/home" => "pages#home", as: :home
+  get "/dashboard" => "dashboard#dashboard_home", as: :dashboard_home
 
   root to: "pages#home"
 end
