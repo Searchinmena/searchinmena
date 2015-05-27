4076d4c2b35120543dfd737b690cc0a4e7e47fcf
Julia Jacobs <julia@llp.pl>
2015-05-27 11:54:49 +0200

    [#1750853] Tweak User#category_name spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,9 @@ describe User do
   end
 
   describe "#category_name" do
-    it "looks for appropriate translation" do
-      expect(I18n).to receive(:t).with("user.types.seller")
-      User.category_name(:seller)
+    it "returns appropriate translation" do
+      expect(I18n).to receive(:t).with("user.types.seller").and_return("seller")
+      expect(User.category_name(:seller)).to eq("seller")
     end
   end
 end
