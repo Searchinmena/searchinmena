42eb2f0124472dd58fddff005aa4c170106d39b2
Julia Jacobs <julia@llp.pl>
2015-05-27 16:29:10 +0200

    [#1750852] Configure fb omniauth provider, add link

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+         :trackable, :validatable, :omniauthable,
+         omniauth_providers: [:facebook]
 
   enum category: [:seller, :buyer, :both]
 
