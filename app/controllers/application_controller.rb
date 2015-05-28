c1a19bef2546a9006cc110ca69e4128046c91df7
mosab <mosab_ah@yahoo.com>
2015-05-28 14:41:07 +0300

    [#1760485] default lang when the param is empty, and adding the param to all the links within the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] if params[:locale]
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    { locale: I18n.locale }
   end
 
   extend Dependor::Injectable
