d72d8a1edd5d91182eb1b67eb46e5ec7c4b2aca7
Julia Jacobs <julia@llp.pl>
2015-05-28 15:56:10 +0200

    [#1750852] Fix omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
+  devise_for :users, skip: [:session, :password, :registration, :confirmation],
+             controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
   scope "(:locale)", locale: /en|ar/ do
-    devise_for :users, controllers: {
-      registrations: "registrations",
-      omniauth_callbacks: "users/omniauth_callbacks"
+    devise_for :users, skip: [:omniauth_callbacks], controllers: {
+      registrations: "registrations"
     }
 
     get "/home" => "pages#home", as: :home
