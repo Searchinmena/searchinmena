5c6809765049767d63d3e9fc06b644e6a3a58618
Julia Jacobs <julia@llp.pl>
2015-05-28 16:08:59 +0200

    [#1750852] Syntax/alignment fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
-  devise_for :users, skip: [:session, :password, :registration, :confirmation],
-             controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users,
+    skip: [:session, :password, :registration, :confirmation],
+    controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   scope "(:locale)", locale: /en|ar/ do
     devise_for :users, skip: [:omniauth_callbacks], controllers: {
