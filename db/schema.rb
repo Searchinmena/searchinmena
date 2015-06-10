a7cde0ad3ac6c10c52b3045496d2b4016e0b23c7
mosab <mosab_ah@yahoo.com>
2015-06-10 16:35:38 +0300

    [#1770918] push new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609130002) do
+ActiveRecord::Schema.define(version: 20150610123118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attributes", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "value",      null: false
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name",          null: false
@@ -46,14 +54,41 @@ ActiveRecord::Schema.define(version: 20150609130002) do
 
   add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
+  create_table "photos", force: :cascade do |t|
+    t.string   "url",        null: false
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
-    t.string   "name",         null: false
+    t.string   "name",                      null: false
     t.string   "model_number"
     t.string   "brand_name"
     t.string   "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "min_order_quantity_number"
+    t.string   "min_order_quantity_unit"
+    t.integer  "FOB_price"
+    t.string   "FOB_price_type"
+    t.string   "FOB_price_unit"
+    t.string   "port"
+    t.string   "payment_terms"
+    t.integer  "supply_abbility_number"
+    t.string   "supply_abbility_type"
+    t.string   "supply_abbility_capacity"
+    t.string   "packaging_details"
   end
+
+  create_table "products_categories", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "products_categories", ["product_id", "category_id"], name: "index_products_categories_on_product_id_and_category_id", unique: true, using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string   "name",       null: false
