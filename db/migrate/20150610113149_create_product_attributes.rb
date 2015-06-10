0f9ec29d076db53cbd968957aba3ba4126ec3d8e
mosab <mosab_ah@yahoo.com>
2015-06-10 17:18:54 +0300

    [#1770918] fixing indentation

--- db/migrate/20150610113149_create_product_attributes.rb
+++ db/migrate/20150610113149_create_product_attributes.rb
@@ -8,6 +8,6 @@ class CreateProductAttributes < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-      add_index :product_attributes, :product_id
+    add_index :product_attributes, :product_id
   end
 end
