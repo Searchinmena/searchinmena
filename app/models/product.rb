61398c899cb3b86269a574a92e5e135cb69b00b3
mosab <mosab_ah@yahoo.com>
2015-06-10 16:33:43 +0300

    [#1770918] link product to photos and attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :business
-
+  has_many :photos
+  has_many :attributes
   has_and_belongs_to_many :categories
+
+  accepts_nested_attributes_for :photos, allow_destroy: true
+  accepts_nested_attributes_for :attributes, allow_destroy: true
 end
