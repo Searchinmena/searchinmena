9bb4fabbc78087c7fb00d90e2a033454714c5641
mosab <mosab_ah@yahoo.com>
2015-06-10 16:31:36 +0300

    [#1770918] create products categories relation table

---
+++ db/migrate/20150610123118_create_products_categories.rb
@@ -0,0 +1,12 @@
+class CreateProductsCategories < ActiveRecord::Migration
+  def change
+    create_table :products_categories do |t|
+      t.integer :product_id
+      t.integer :category_id
+
+      t.timestamps null: false
+    end
+
+    add_index :products_categories, [:product_id, :category_id], unique: true
+  end
+end
