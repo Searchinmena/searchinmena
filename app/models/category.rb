bc628b5bc4dec82f35ba19126065a7533b45faae
mosab <mosab_ah@yahoo.com>
2015-06-10 16:32:33 +0300

    [#1770918] create parent relation for categories and sub categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :products
+  belongs_to :parent, class_name: 'Category', foreign_key: :parent_id
 
   default_scope { order("LOWER(name)") }
+
+  def parent?
+    parent.nil?
+  end
 end
