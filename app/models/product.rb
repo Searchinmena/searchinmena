7b0aa2881afd00a6bf0dd9141a156c405f28ff4b
mosab <mosab_ah@yahoo.com>
2015-06-10 17:04:36 +0300

    [#1770918] change photo to product photo and attribute to product attribute

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
   belongs_to :business
-  has_many :photos
-  has_many :attributes
   has_and_belongs_to_many :categories
+  has_many :product_photos
+  has_many :product_attributes
 
-  accepts_nested_attributes_for :photos, allow_destroy: true
-  accepts_nested_attributes_for :attributes, allow_destroy: true
+  accepts_nested_attributes_for :product_photos, allow_destroy: true
+  accepts_nested_attributes_for :product_attributes, allow_destroy: true
 end
