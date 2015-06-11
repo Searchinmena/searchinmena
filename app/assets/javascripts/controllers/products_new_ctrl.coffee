3b2e90e6d23cb26f7372a48d60ab0ef309236677
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-11 13:37:06 +0200

    [#1770896] Back to I18n.t

--- app/assets/javascripts/controllers/products_new_ctrl.coffee
+++ app/assets/javascripts/controllers/products_new_ctrl.coffee
@@ -1,4 +1,9 @@
 @Sim.controller 'ProductsNewCtrl', ['$scope'
   ($scope) ->
+    $scope.submit = (e) ->
+      e.preventDefault()
 
+      console.log($scope.form)
+
+      false
 ]
--- config/initializers/angular_rails_templates.rb
+++ config/initializers/angular_rails_templates.rb
@@ -1,18 +1,1 @@
-module Sim
-  class HamlTemplateWithRailsHelpers < Tilt::HamlTemplate
-    def evaluate(scope, locals, &block)
-      scope.class_eval do
-        include Rails.application.routes.url_helpers
-        include Rails.application.routes.mounted_helpers
-        include ActionView::Helpers
-      end
 
-      super
-    end
-  end
-end
-
-Tilt.register Sim::HamlTemplateWithRailsHelpers, '.haml'
-
-Rails.application.assets.register_engine '.haml',
-  Sim::HamlTemplateWithRailsHelpers
