745a75fe7c55e8cbbbc665ca42e75616cb43c225
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-17 13:18:31 +0200

    Fixed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,10 @@ seeds.each do |seed, repository|
 end
 
 # user for e2e tests
-require 'factory_girl'
-require './spec/support/factories'
-unless User.where(email: TEST_EMAIL).count > 0
-  FactoryGirl.create(:user, email: TEST_EMAIL, password: TEST_PASSWORD)
+if Rails.env.test?
+  require 'factory_girl'
+  require './spec/support/factories'
+  unless User.where(email: TEST_EMAIL).count > 0
+    FactoryGirl.create(:user, email: TEST_EMAIL, password: TEST_PASSWORD)
+  end
 end
