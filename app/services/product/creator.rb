c6c8a833c6c8b957f88089b75d7962b0240f0051
mosab <mosab_ah@yahoo.com>
2015-06-19 14:41:43 +0300

    [#1770918] link product creator and storer together

--- app/services/product/creator.rb
+++ app/services/product/creator.rb
@@ -1,10 +1,11 @@
 class Product::Creator < BaseService
-  inject :product_repository
+  inject :product_repository, :product_storer
 
   takes :new_product_params
 
   def perform
     product = product_repository.new(new_product_params[:product])
+    storer = product_storer.new(product ,new_product_params[:product])
     storer.perform(product)
   end
 end
