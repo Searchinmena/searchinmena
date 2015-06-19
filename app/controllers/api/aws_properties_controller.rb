cc722556a781d43312ca8cc576ea13360b17cd03
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-19 09:51:43 +0200

    [#1778491] Make upload more secure

--- app/controllers/api/aws_properties_controller.rb
+++ app/controllers/api/aws_properties_controller.rb
@@ -2,8 +2,18 @@ class Api::AwsPropertiesController < Api::BaseController
   inject :policy_builder, :signature_builder
 
   def index
+    return head :unauthorized unless authorized?
+
     policy = policy_builder.call
     signature = signature_builder.call(policy)
     render json: { policy: policy, signature: signature }
+  end
+
+  private
+
+  def authorized?
+    return true unless Rails.env.production?
+
+    request.headers['origin'] == A9n.app_host
   end
 end
--- app/services/policy_builder.rb
+++ app/services/policy_builder.rb
@@ -1,12 +1,32 @@
 require 'base64'
 
 class PolicyBuilder
+  EXPIRE_INTERVAL = 3.seconds
+  DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+  MAX_CONTENT_LENGTH = 524_288_000
+  KEY_STARTS_WITH = "photos/"
+
   def call
-    policy_document = File.read(policy_path)
-    Base64.encode64(policy_document).gsub("\n", "")
+    policy = policy_hash.to_json
+    Base64.encode64(policy).gsub("\n", "")
   end
 
-  def policy_path
-    Rails.root.join("config", "aws", "policy.#{A9n.env}.json")
+  def policy_hash
+    {
+      expiration: expiration,
+      conditions: [
+        { bucket: A9n.aws[:bucket_name] },
+        ["starts-with", "$key", KEY_STARTS_WITH],
+        { acl: "private" },
+        ["starts-with", "$Content-Type", ""],
+        ["starts-with", "$filename", ""],
+        ["content-length-range", 0, MAX_CONTENT_LENGTH]
+      ]
+    }
+  end
+
+  def expiration
+    date = Time.now + EXPIRE_INTERVAL
+    date.strftime(DATE_FORMAT)
   end
 end
