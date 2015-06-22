bac8ddadcec4f01eba06f21ba9a36de6a5317d19
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-22 10:24:11 +0200

    [#1770896] Order selects by id

--- app/repositories/translatable_repository.rb
+++ app/repositories/translatable_repository.rb
@@ -1,6 +1,6 @@
 class TranslatableRepository < AbstractRepository
   def all_with_translations(locale)
     klass.joins(:translations).includes(:translations)
-      .where("translations.locale": locale)
+      .where("translations.locale": locale).order(:id)
   end
 end
