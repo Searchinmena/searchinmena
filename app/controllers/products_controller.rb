28a6806ccf07932826f472e5183f0830346468e3
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-23 13:47:45 +0200

    [#1783228] Moved ProductsController out of api scope + cleanup

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,43 @@
 class ProductsController < ApplicationController
+  inject :product_repository
+
   def create
-    head :ok
+    product_creator = Product::Creator.new(new_product_params)
+    response = product_creator.perform
+    if response.successful?
+      flash[:notice] = t("en.product.added")
+      head :ok
+    else
+      render_error(response.product)
+    end
+  end
+
+  private
+
+  def render_error(product)
+    render json: {
+      product: ErrorsPresenter.new(product)
+    }, status: :conflict
+  end
+
+  def new_product_params
+    {
+      product: product_params,
+      trade_info: trade_info_params
+    }
+  end
+
+  def product_params
+    params[:product]
+      .permit([:name, :model_number, :brand_name, :description, :category])
+  end
+
+  def trade_info_params
+    params[:product]
+      .permit([:min_order_quantity_number, :min_order_quantity_unit,
+               :fob_price, :fob_price_type, :fob_price_unit, :port,
+               :payment_terms, :supply_abbility_number,
+               :supply_abbility_type, :upply_abbility_capacity,
+               :packaging_details])
   end
 end
