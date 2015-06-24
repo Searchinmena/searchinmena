6442cf9190ed1f18cb94be7bd3216bd42b505b8a
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-24 10:09:45 +0200

    [#1783228] Product model cleanup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :business
   has_one :category
-  has_many :photos, class_name: 'ProductPhoto'
-  has_many :product_attributes
-
-  accepts_nested_attributes_for :product_attributes, allow_destroy: true
+  has_many :photos, class_name: 'ProductPhoto', dependent: :delete_all
+  has_many :product_attributes, dependent: :delete_all
 end
