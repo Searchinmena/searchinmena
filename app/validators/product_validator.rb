39e3884874544f8ad15bf21ba351aecddd805016
mosab <mosab_ah@yahoo.com>
2015-06-26 08:53:42 +0300

    [#1770918] add validation and price_format to config

--- app/validators/product_validator.rb
+++ app/validators/product_validator.rb
@@ -7,4 +7,10 @@ class ProductValidator < BaseValidator
 
   validates :name, :model_number, :brand_name,
             :description, :category_id, presence: true
+  validates :name, :model_number, :brand_name,
+            length: { maximum: A9n.validations[:max_text_field_size] }
+  validates :description,
+            length: { maximum: A9n.validations[:max_text_area_size] }
+  validates :model_number,
+            numericality: { only_integer: true }
 end
