f76a8708ce13cda0ab998eb649435a06107838c5
mosab <mosab_ah@yahoo.com>
2015-06-26 08:26:14 +0300

    [#1770918] cleaning the files

--- app/services/product/product_storer.rb
+++
@@ -1,7 +0,0 @@
-class Product::ProductStorer < Product::Storer
-  def initialize(product, product_params)
-    product_creator = Product::ProductCreator.new(product, product_params)
-    self.handlers = [product_creator]
-  end
-end
-
