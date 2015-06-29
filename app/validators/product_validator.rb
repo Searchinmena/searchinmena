48ed3d391d2b3d5bfd221519c03473e3a45c18f8
mosab <mosab_ah@yahoo.com>
2015-06-29 12:06:34 +0300

    [#1770918] adding validations to product

--- app/validators/product_validator.rb
+++ app/validators/product_validator.rb
@@ -5,12 +5,19 @@ class ProductValidator < BaseValidator
 
   attr_accessor(*fields)
 
-  validates :name, :model_number, :brand_name,
-            :description, :category_id, presence: true
-  validates :name, :model_number, :brand_name,
+  validates :name, :category_id, presence: true
+  validates :name, :model_number, :brand_name, :min_order_quantity_number,
+            :min_order_quantity_unit, :fob_price, :fob_price_currency,
+            :fob_price_unit, :port, :payment_terms, :supply_abbility_capacity,
+            :supply_abbility_unit, :supply_abbility_frequency,
+            :packaging_details,
             length: { maximum: A9n.validations[:max_text_field_size] }
   validates :description,
             length: { maximum: A9n.validations[:max_text_area_size] }
-  validates :model_number,
+  validates :model_number, :category_id, :min_order_quantity_number,
+            :supply_abbility_capacity,
             numericality: { only_integer: true }
+  validates :fob_price,
+            numericality: { greater_than: 0 },
+            format: { with: /#{A9n.validations[:price_format]}/ }
 end
