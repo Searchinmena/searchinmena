c680f8ad389e8a8aa2fa9f58fb3c322eda3b6f80
Ania Slimak <anna.slimak@lunarlogic.io>
2015-06-29 13:07:33 +0200

    [#1784194, #1783671] Display errors on resend password instructions page (also in flash)

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,5 +1,11 @@
 module Users
   class ConfirmationsController < Devise::ConfirmationsController
+    def create
+      super do
+        flash[:error] = full_error_message_for(resource)
+      end
+    end
+
     protected
 
     def after_resending_confirmation_instructions_path_for(_resource_name)
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,28 +1,9 @@
 module Users
   class PasswordsController < Devise::PasswordsController
     def create
-      self.resource = resource_class
-        .send_reset_password_instructions(resource_params)
-      yield resource if block_given?
-
-      if successfully_sent?(resource)
-        respond_to_success
-      else
-        respond_to_error
+      super do
+        flash[:error] = full_error_message_for(resource)
       end
-    end
-
-    private
-
-    def respond_to_success
-      location = after_sending_reset_password_instructions_path_for(
-        resource_name)
-      respond_with({}, { location: location })
-    end
-
-    def respond_to_error
-      flash[:error] = resource.errors.full_messages.first
-      respond_with(resource)
     end
   end
 end
