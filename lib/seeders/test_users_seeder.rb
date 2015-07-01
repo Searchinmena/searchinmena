b303c5796f8c5942c8cbb197a826b1fe7301792c
Ania Slimak <anna.slimak@lunarlogic.io>
2015-07-01 11:00:21 +0200

    [#1789493] Require factory_girl only for non production ends

--- lib/seeders/test_users_seeder.rb
+++ lib/seeders/test_users_seeder.rb
@@ -1,9 +1,9 @@
-require 'factory_girl'
-require './spec/support/factories'
-
 class TestUsersSeeder
   def seed
     return if Rails.env.production?
+
+    require 'factory_girl'
+    require './spec/support/factories'
 
     unless User.where(email: TEST_EMAIL).count > 0
       FactoryGirl.create(:user, email: TEST_EMAIL, password: TEST_PASSWORD)
