dd5a9d3b7bb5f04d6cffbe6ae5855d1fe07daeed
Ania Slimak <anna.slimak@lunarlogic.io>
2015-07-01 12:29:26 +0200

    Fixed category relation in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :business
-  has_one :category
+  belongs_to :category
   has_many :photos, class_name: 'ProductPhoto', dependent: :delete_all
   has_many :product_attributes, dependent: :delete_all
 end
