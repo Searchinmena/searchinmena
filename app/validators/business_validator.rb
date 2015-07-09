a26b13b29ad08621810632932939dec9bdaa9eb2
Julia Jacobs <julia@llp.pl>
2015-07-09 17:26:31 +0200

    [#1787659] Syntax change

--- app/validators/business_validator.rb
+++ app/validators/business_validator.rb
@@ -9,7 +9,6 @@ class BusinessValidator < BaseValidator
   validates :name, :country_id, :phone, :business_type_ids, presence: true
   validates :name, :country_id, :phone, :address_line_1, :address_line_2,
             length: { maximum: A9n.validations[:max_text_field_size] }
-  validates :no_of_employees, :year_registered, numericality: { greater_than: 0,
-                                                            only_integer: true,
-                                                            allow_nil: true }
+  validates :no_of_employees, :year_registered, numericality:
+            { greater_than: 0, only_integer: true, allow_nil: true }
 end
