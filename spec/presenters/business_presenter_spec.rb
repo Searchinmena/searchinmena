ab959c04902d1a1204b3af96b4aa035a5f8e99ff
Julia Jacobs <julia@llp.pl>
2015-07-09 16:52:56 +0200

    [#1787659] Add BusinessPresenter spec

---
+++ spec/presenters/business_presenter_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe BusinessPresenter do
+  describe "#as_json" do
+    let(:presenter) { BusinessPresenter.new(business) }
+    let(:business) { build(:business) }
+
+    subject { presenter.as_json }
+
+    it "includes prefilled attributes" do
+      is_expected.to include(
+        name: business.name,
+        phone: business.phone,
+        country_id: business.country_id,
+        year_registered: business.year_registered,
+        no_of_employees: business.no_of_employees,
+        introduction: business.introduction,
+        address_line_1: business.address_line_1,
+        address_line_2: business.address_line_2,
+        business_type_ids: business.business_type_ids
+      )
+    end
+  end
+end
