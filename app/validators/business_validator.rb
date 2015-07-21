a0fcc6cfa7e1a6e3fc0e2ffc9606d614aaa7d494
Julia Jacobs <julia@llp.pl>
2015-07-21 15:10:50 +0200

    [#1787659] Nicer breaks in business validator

--- app/validators/business_validator.rb
+++ app/validators/business_validator.rb
@@ -8,10 +8,11 @@ class BusinessValidator < BaseValidator
 
   validates :name, :country_id, :phone, :business_type_ids, presence: true
   validates :name, :phone, :address_line_1, :address_line_2, :no_of_employees,
-            :year_registered, length: { maximum:
-            A9n.validations[:max_text_field_size] }
-  validates :introduction, length: { maximum:
-            A9n.validations[:max_text_area_size] }
-  validates :no_of_employees, :year_registered, numericality:
-            { greater_than: 0, only_integer: true, allow_nil: true }
+            :year_registered,
+            length: { maximum: A9n.validations[:max_text_field_size] }
+  validates :introduction,
+            length: { maximum: A9n.validations[:max_text_area_size] }
+  validates :no_of_employees, :year_registered,
+            numericality: { greater_than: 0, only_integer: true,
+                            allow_nil: true }
 end
