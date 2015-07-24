4f5fcd21680bfddc8f66c1d2c29aa897c10c57cf
Julia Jacobs <julia@llp.pl>
2015-07-24 14:45:15 +0200

    Remove obsolete method

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -4,10 +4,4 @@ class BasePresenter
   def injector
     @injector ||= PresentersInjector.new
   end
-
-  def formatted_price(price)
-    return "" unless price
-
-    format("%.2f", price)
-  end
 end
