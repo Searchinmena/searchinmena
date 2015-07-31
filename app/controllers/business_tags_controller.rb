64b9f905ed40412c4b358da35d1086a4a3b0ec67
ciembor <pub@ciemborowicz.pl>
2015-07-31 14:44:30 +0200

    Shut Rubocop up.

--- app/controllers/business_tags_controller.rb
+++ app/controllers/business_tags_controller.rb
@@ -2,7 +2,9 @@ class BusinessTagsController < TranslatableController
   inject :tag_repository
 
   def index
-    tags = tag_repository.for_business_with_translations(params[:business_id], params[:locale])
+    tags = tag_repository.for_business_with_translations(
+      params[:business_id], params[:locale]
+    )
     render json: tags.map { |t| TranslatablePresenter.new(t) }
   end
 end
