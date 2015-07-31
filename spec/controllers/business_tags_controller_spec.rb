ef72a8a071b03eb15f741f710006ee7772ffb96d
ciembor <pub@ciemborowicz.pl>
2015-07-31 16:26:45 +0200

    More expectations in business tags controller.

--- spec/controllers/business_tags_controller_spec.rb
+++ spec/controllers/business_tags_controller_spec.rb
@@ -3,9 +3,19 @@ require "rails_helper"
 describe BusinessTagsController do
   describe "#index" do
     let(:business) { create(:business, tags: [create(:tag)]) }
+    let(:tag_repository) { double }
+    let(:translatable) { double }
+    let(:locale) { 'en' }
 
     it "is successful" do
-      get :index, locale: 'en', business_id: business.id
+      expect(controller).to receive(:tag_repository).and_return(tag_repository)
+      expect(tag_repository).to receive(:for_business_with_translations)
+        .with(business.id.to_s, locale)
+        .and_return([translatable])
+      expect(TranslatablePresenter).to receive(:new).with(translatable)
+        .and_return({})   
+
+      get :index, locale: locale, business_id: business.id
       expect(response).to be_successful
     end
   end
