1daca906b105f0f0724013da5cc53e65850d3c73
Ania Slimak <anna.slimak@lunarlogic.io>
2015-08-03 11:33:05 +0200

    [#1809893] Added validation to attribtues

--- app/validators/attribute_validator.rb
+++ app/validators/attribute_validator.rb
@@ -4,4 +4,9 @@ class AttributeValidator < BaseValidator
   end
 
   attr_accessor(*fields)
+
+  validates :name, :value,
+            length: { maximum: A9n.validations[:max_text_field_size] }
+
+  validates :value, presence: true, if: proc { |i| i.name.present? }
 end
---
+++ spec/validators/attribute_validator_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe AttributeValidator do
+  subject { described_class.new(attribute_params) }
+
+  let(:attribute_params) { valid_params }
+  let(:valid_params) { build_params(build(:product_attribute)) }
+
+  describe "length validation" do
+    [:name, :value].each do|field|
+      let(:attribute_params) do
+        too_long_field = "a" * (A9n.validations[:max_text_field_size] + 1)
+        valid_params.merge(field => too_long_field)
+      end
+
+      it "requires #{field} to be shorter than
+        #{A9n.validations[:max_text_field_size]}" do
+        is_expected.to be_invalid
+      end
+    end
+  end
+
+  describe "#value" do
+    context "name is blank" do
+      let(:attribute_params) { valid_params.merge(name: nil) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "name is present but value not" do
+      let(:attribute_params) do
+        valid_params.merge(name: "attribute", value: nil)
+      end
+
+      it { is_expected.to be_invalid }
+    end
+  end
+end
+
