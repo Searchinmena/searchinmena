2617108182e0c63f2d24ab1222b0d05bdccdfbc0
ciembor <pub@ciemborowicz.pl>
2015-08-04 17:22:46 +0200

    [#1812775] Device password validations instead of custom validations.
    [#1812775] Too long line.
    
    [#1812775] Trailing spaces.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :validatable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :trackable, :confirmable, :omniauthable, omniauth_providers:
-         [:facebook]
+  # :lockable and :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :confirmable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   enum category: [:seller, :buyer, :both]
 
   has_one :business, dependent: :delete
+
+  validates_confirmation_of :password
 
   def self.category_name(type)
     I18n.t("user.types.#{type}")
