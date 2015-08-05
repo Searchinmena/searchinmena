64f59f1b938c964d55b82aa605ce8ad088d46a0e
ciembor <pub@ciemborowicz.pl>
2015-08-05 19:28:47 +0200

    [#1770986] Test for business types in tag repository.

--- spec/repositories/business_type_repository_spec.rb
+++ spec/repositories/business_type_repository_spec.rb
@@ -2,6 +2,25 @@ require "rails_helper"
 
 describe BusinessTypeRepository do
   let(:repository) { BusinessTypeRepository.new }
+  let(:locale) { 'en' }
 
   it_behaves_like "TranslatableRepository"
+
+  def business_type_with_translation(value, locale)
+    translation = create(:translation, value: value, locale: locale)
+    create(:business_type_with_translation, translation: translation)
+  end
+
+  describe "#for_business_with_translations" do
+    let!(:not_matching_business_type) { business_type_with_translation('Not Matching', 'en') }
+    let!(:business_types) { [business_type_with_translation('Matching', 'en')] }
+    let(:business) { create(:business, business_types: business_types) }
+
+    subject { repository.for_business_with_translations(business.id, locale) }
+
+    it "should return translated type for business" do
+      is_expected.to eq(business_types)
+      is_expected.not_to include(not_matching_business_type)
+    end
+  end
 end
