0b37af4f208e753cf5537cfee8fb0e80255a7273
Julia Jacobs <julia@llp.pl>
2015-08-19 13:23:52 +0200

    Fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require "rails_helper"
 describe User do
   describe "#category_name" do
     it "returns appropriate translation" do
-      expect(I18n).to receive(:t).with("user.types.seller").and_return("seller")
+      expect(I18n).to receive(:t).with("user.categories.seller")
+        .and_return("seller")
       expect(User.category_name(:seller)).to eq("seller")
     end
   end
