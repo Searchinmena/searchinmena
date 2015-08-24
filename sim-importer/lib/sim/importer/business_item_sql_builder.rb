2c6347ecb87fee9715b977375e78020181824b3c
Ania Slimak <anna.slimak@lunarlogic.io>
2015-08-24 13:03:46 +0200

    [#1822508] Rubocop...

--- sim-importer/lib/sim/importer/business_item_sql_builder.rb
+++ sim-importer/lib/sim/importer/business_item_sql_builder.rb
@@ -2,11 +2,11 @@ module Sim
   module Importer
     class BusinessItemSqlBuilder < StandardSqlBuilder
       def additional_columns
-        ["created_at", "updated_at"]
+        %w{created_at updated_at}
       end
 
       def additional_values
-        ['NOW()', 'NOW()']
+        %w{NOW() NOW()}
       end
     end
   end
