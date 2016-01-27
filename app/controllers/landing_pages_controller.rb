30f35cbe7a1c0d400a699ac34df3df6665346e5c
shahidbscs <shahidbscs@gmail.com>
2016-01-27 11:52:29 +0400

    Remove the extra spaces from the landing_pages_controller.rb files and pass guard test

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -6,16 +6,14 @@ class LandingPagesController < ApplicationController
       redirect_to '/#/dashboard'
       return
     end
-    
     user_params = user_params_factory.build(session["devise.auth_data"])
     user = user_repository.new(user_params)
     business = business_repository.new
     session["devise.auth_data"] = nil
     render :home_one, locals:
-       {
-         user: user, business: business,
-         user_presenter: UserPresenter.new(user)
-
-       }
+      {
+        user: user, business: business,
+        user_presenter: UserPresenter.new(user)
+      }
   end
 end
