0ef846b8c748f7b0d8031721a6317abddc1cce7f
shahidbscs <shahidbscs@gmail.com>
2016-02-10 10:44:00 +0400

    Add cron job for sitemap refresh every day and capistrano task to refresh sitemap after successfull deployment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 set :output, "#{path}/log/cron.log"
 
-every 1.days, at: '1 AM' do
+every 1.day, at: '1:00 am' do
   rake 'delete_tmp_files'
+end
+
+every 1.day, at: '3:00 am' do
+  rake '-s sitemap:refresh'
 end
