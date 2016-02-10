d83d08e8e63ad73c606cd49a0a303a499bd13b01
shahidbscs <shahidbscs@gmail.com>
2016-02-10 12:10:57 +0400

    Remove conflict from Gemfile

---
+++ app/services/customer_io_service.rb
@@ -0,0 +1,102 @@
+class CustomerIoService
+  def cio(cio_event, parm)
+    send cio_event, cio_event, parm
+  end
+
+  private
+
+  def identify_user(parm)
+    $customerio.identify(
+      id: parm.id,
+      email: parm.email,
+      created_at: parm.created_at.to_i,
+      first_name: parm.first_name,
+      last_name: parm.last_name,
+      sign_in_count: parm.sign_in_count,
+      category: parm.category
+    )
+  end
+
+  def user_signup(cio_event, parm)
+    identify_user(parm)
+    $customerio.track(parm.id, cio_event)
+  end
+
+  def user_just_login(cio_event, parm)
+    identify_user(parm)
+    $customerio.track(parm.id, cio_event)
+  end
+
+  def user_updated_company(cio_event, parm)
+    company_info_track(cio_event, parm)
+  end
+
+  def user_add_company(cio_event, parm)
+    company_info_track(cio_event, parm)
+  end
+
+  def company_info_track(cio_event, parm)
+    $customerio.track(
+      parm.user_id,
+      cio_event,
+      company_id: parm.id,
+      company_name: parm.name,
+      #:company_category: ,
+      company_phone_number: parm.phone,
+      company_country: parm.country_id,
+      company_city: parm.city,
+      company_profile_pic: parm.logo,
+      company_created_at: parm.created_at.to_i,
+      company_updated_at: parm.updated_at.to_i,
+      company_year_registered: parm.year_registered,
+      company_no_of_employees: parm.no_of_employees,
+      company_introduction: parm.introduction,
+      company_address_line_1: parm.address_line_1,
+      company_address_line_2: parm.address_line_2
+    )
+  end
+
+  def user_add_product(cio_event, p)
+    object = is_object_exist(p['response'].object)
+    uid = p['user'].id
+    attributes = attribute_exist(p['response'])
+    user_add_product_track(uid, cio_event, object, attributes)
+  end
+
+  def attribute_exist(response)
+    if response.attributes.present?
+      parm_attr = response.attributes.first
+      attributes = { name: parm_attr['name'], value: parm_attr['value'] }
+    else
+      attributes = { name: '', value: '' }
+    end
+    attributes
+  end
+
+  def user_add_product_track(uid, cio_event, parm, attributes)
+    $customerio.track(
+      uid, cio_event,
+      product_id: parm.id,
+      company_name: parm.name,
+      model_number: parm.model_number,
+      brand_name: parm.brand_name,
+      description: parm.description,
+      category_id: parm.category_id,
+      created_at: parm.created_at.to_i,
+      updated_at: parm.updated_at.to_i,
+      min_order_quantity_number: parm.min_order_quantity_number,
+      min_order_quantity_unit_id: parm.min_order_quantity_unit_id,
+      fob_price: parm.fob_price,
+      fob_price_currency_id: parm.fob_price_currency_id,
+      fob_price_unit_id: parm.fob_price_unit_id,
+      port: parm.port,
+      supply_ability_capacity: parm.supply_ability_capacity,
+      supply_ability_unit_id: parm.supply_ability_unit_id,
+      supply_ability_frequency_id: parm.supply_ability_frequency_id,
+      packaging_details: parm.packaging_details,
+      business_id: parm.business_id,
+      characteristics_attr_name: attributes['name'],
+      characteristics_attr_value: attributes['value']
+    )
+  end
+end
