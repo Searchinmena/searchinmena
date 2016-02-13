e3a3a21845b0b518f1fc78581bf1421e91124b1b
shahidbscs <shahidbscs@gmail.com>
2016-02-13 20:25:44 +0400

    Add customer.io integration setting into configuration files

--- app/services/customer_io_service.rb
+++ app/services/customer_io_service.rb
@@ -1,7 +1,6 @@
 class CustomerIoService < Customerio::Client
   def initialize(parm, cio_event)
-    @cio = Customerio::Client.new("3823999354dd63de1f8d",
-                                  "1a4d2164782635ea53b1", json: true)
+    @cio = Customerio::Client.new(A9n.cio[:id], A9n.cio[:key], json: true)
     send cio_event, parm, cio_event
   end
 
@@ -34,92 +33,22 @@ class CustomerIoService < Customerio::Client
   end
 
   def user_updated_company(parm, cio_event)
-    event_attributes = company_info_attr(parm[:response])
+    event_attributes = _company_info_attr(parm[:response])
     if parm[:response].updated_at.to_i == parm[:response].created_at.to_i
       cio_event = 'user_add_company'
     end
     track_event(parm[:user].id, cio_event, event_attributes)
   end
 
-  def company_info_attr(parm)
-    {
-      company_name: parm.name,
-      #:company_category: ,
-      company_phone_number: parm.phone,
-      company_country: parm.country_id,
-      company_city: parm.city,
-      company_profile_pic: parm.logo,
-      company_created_at: parm.created_at.to_i,
-      company_updated_at: parm.updated_at.to_i,
-      company_year_registered: parm.year_registered,
-      company_no_of_employees: parm.no_of_employees,
-      company_introduction: parm.introduction,
-      company_address_line_1: parm.address_line_1,
-      company_address_line_2: parm.address_line_2
-    }
-  end
-
   def business_item_update(parm, cio_event)
     object = parm[:response].object
-    product_attributes = pro_serv_attr(object)
-    attributes = pro_serv_order_attr(object).merge!(product_attributes)
-    event_attributes = item_detail_attr(object).merge!(attributes)
+    product_attributes = _pro_serv_attr(object)
+    attributes = _pro_serv_order_attr(object).merge!(product_attributes)
+    event_attributes = _item_detail_attr(object).merge!(attributes)
     if object.updated_at.to_i == object.created_at.to_i
       cio_event = 'business_item_added'
     end
     track_event(parm[:user].id, cio_event, event_attributes)
-  end
-
-  def pro_serv_attr(parm)
-    {
-      product_id: parm.id,
-      product_name: parm.name,
-      created_at: parm.created_at.to_i,
-      updated_at: parm.updated_at.to_i,
-      description: parm.description,
-      category_id: parm.category_id
-    }
-  end
-
-  def pro_serv_order_attr(parm)
-    {
-      fob_price: parm.fob_price,
-      fob_price_currency_id: parm.fob_price_currency_id,
-      fob_price_unit_id: parm.fob_price_unit_id,
-      port: parm.port,
-      supply_capacity: parm.supply_ability_capacity,
-      supply_unit: parm.supply_ability_unit_id,
-      supply_frequency: parm.supply_ability_frequency_id,
-      packaging_details: parm.packaging_details,
-      business_id: parm.business_id,
-      characteristics_attr_name: tags_attr(parm, ':name'),
-      characteristics_attr_value: tags_attr(parm, ':value')
-    }
-  end
-
-  def item_detail_attr(parm)
-    {
-      place_of_origin: attribute_exist(parm, ':place_of_origin'),
-      scope_of_work: attribute_exist(parm, 'scope_of_work'),
-      completion_time: attribute_exist(parm, 'average_completion_time'),
-      completion_unit: attribute_exist(parm, 'average_completion_time_unit_id'),
-      model_number: attribute_exist(parm, 'model_number'),
-      brand_name: attribute_exist(parm, 'brand_name'),
-      min_order: attribute_exist(parm, 'min_order_quantity_number'),
-      min_order_unit: attribute_exist(parm, 'min_order_quantity_unit_id')
-    }
-  end
-
-  def tags_attr(parm, attr_name)
-    if defined? parm.attributes
-      attribute_exist(parm.attributes, attr_name)
-    end
-  end
-
-  def attribute_exist(parm, attr_name)
-    if parm.present?
-      parm[attr_name].present? ? parm[attr_name] : nil
-    end
   end
 
   def reset_password_request(parm, cio_event)
@@ -140,11 +69,81 @@ class CustomerIoService < Customerio::Client
     params = parm[:params]
     user = parm[:user]
     business = Business.find_by_id(params[:business_id])
-    event_attributes = message_company_attr(params, user, business)
+    event_attributes = _message_company_attr(params, user, business)
     track_event(user.id, cio_event, event_attributes)
   end
 
-  def message_company_attr(parm, user, business)
+  def _company_info_attr(parm)
+    {
+      company_name: parm.name,
+      #:company_category: ,
+      company_phone_number: parm.phone,
+      company_country: parm.country_id,
+      company_city: parm.city,
+      company_profile_pic: parm.logo,
+      company_created_at: parm.created_at.to_i,
+      company_updated_at: parm.updated_at.to_i,
+      company_year_registered: parm.year_registered,
+      company_no_of_employees: parm.no_of_employees,
+      company_introduction: parm.introduction,
+      company_address_line_1: parm.address_line_1,
+      company_address_line_2: parm.address_line_2
+    }
+  end
+
+  def _pro_serv_attr(parm)
+    {
+      product_id: parm.id,
+      product_name: parm.name,
+      created_at: parm.created_at.to_i,
+      updated_at: parm.updated_at.to_i,
+      description: parm.description,
+      category_id: parm.category_id
+    }
+  end
+
+  def _pro_serv_order_attr(parm)
+    {
+      fob_price: parm.fob_price,
+      fob_price_currency_id: parm.fob_price_currency_id,
+      fob_price_unit_id: parm.fob_price_unit_id,
+      port: parm.port,
+      supply_capacity: parm.supply_ability_capacity,
+      supply_unit: parm.supply_ability_unit_id,
+      supply_frequency: parm.supply_ability_frequency_id,
+      packaging_details: parm.packaging_details,
+      business_id: parm.business_id,
+      characteristics_attr_name: _tags_attr(parm, ':name'),
+      characteristics_attr_value: _tags_attr(parm, ':value')
+    }
+  end
+
+  def _item_detail_attr(parm)
+    {
+      place_of_origin: _attr_exist(parm, ':place_of_origin'),
+      scope_of_work: _attr_exist(parm, 'scope_of_work'),
+      completion_time: _attr_exist(parm, 'average_completion_time'),
+      completion_unit: _attr_exist(parm, 'average_completion_time_unit_id'),
+      model_number: _attr_exist(parm, 'model_number'),
+      brand_name: _attr_exist(parm, 'brand_name'),
+      min_order: _attr_exist(parm, 'min_order_quantity_number'),
+      min_order_unit: _attr_exist(parm, 'min_order_quantity_unit_id')
+    }
+  end
+
+  def _tags_attr(parm, attr_name)
+    if defined? parm.attributes
+      attribute_exist(parm.attributes, attr_name)
+    end
+  end
+
+  def _attr_exist(parm, attr_name)
+    if parm.present?
+      parm[attr_name].present? ? parm[attr_name] : nil
+    end
+  end
+
+  def _message_company_attr(parm, user, business)
     {
       message_subject: parm[:subject],
       message_description: parm[:body],
