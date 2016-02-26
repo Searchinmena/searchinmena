701ce28440188fc15c028e852b9450676dc193e3
salman aslam <salmanaslam25@gmail.com>
2016-02-26 18:57:39 +0500

    product category and service category

---
+++ app/admin/business.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Business do
+  permit_params :name, :phone, :user_id, :no_of_employees, :country_id, :city
+  form do |f|
+    semantic_errors # shows errors on :base
+    f.inputs do
+      f.input :name
+      f.input :phone
+      f.input :user_id, as: :select, collection:
+      User.all.map { |c| [c.first_name, c.id] }
+      f.input :year_registered
+      f.input :no_of_employees
+      f.input :introduction
+      f.input :country_id, as: :select, collection:
+      Country.all.map { |c| [c.id, c.id] }
+      f.input :address_line_1
+      f.input :address_line_2
+      f.input :city
+      f.input :logo
+      f.input :logo_tmp
+    end
+    actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+end
