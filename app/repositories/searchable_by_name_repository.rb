b61f2a8df0e4b7f034199549a7f27724a4a003ba
shahidbscs <shahidbscs@gmail.com>
2016-03-03 14:33:36 +0400

    Fix the RSpec and gurad test for search features

--- app/repositories/searchable_by_name_repository.rb
+++ app/repositories/searchable_by_name_repository.rb
@@ -5,11 +5,11 @@ module SearchableByNameRepository
       tag_arr = Tag.includes(:translations)
         .where("value ILIKE ?", "%#{keywords}%")
         .pluck('id')
-      klass.includes(:businesses_tags).where("name ILIKE ? or tag_id IN (?)",
-                                             "%#{keywords}%", tag_arr).references(:businesses_tags)
+      klass.includes(:businesses_tags)
+        .where("name ILIKE ? or tag_id IN (?)",
+               "%#{keywords}%", tag_arr).references(:businesses_tags)
     else
       klass.where("name ILIKE ?", "%#{keywords}%")
     end
-
   end
 end
