ca09d6746b919c2ee11d73d72a570251029a5616
salman aslam <salman@clustox.com>
2016-03-15 00:47:38 +0500

    revert the changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,22 +10,4 @@ class User < ActiveRecord::Base
   has_one :business, dependent: :delete
 
   validates_confirmation_of :password
-
-  def self.category_name(type)
-    I18n.t("user.categories.#{type}")
-  end
-
-  def can_see_business_items?
-    seller? || both?
-  end
-
-  def self.associated_users(business)
-    users = if business.user_id.present?
-              Business.where('user_id is NOT NULL and user_id != (?)',
-              business.user_id)
-            else
-              Business.where('user_id is NOT NULL')
-            end
-    User.where.not("id IN (?)", users.map(&:user_id))
-  end
 end
