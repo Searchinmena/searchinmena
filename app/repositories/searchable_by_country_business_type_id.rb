5a590ecbe250097d82de4235c5b28ce069ec8a88
Arvind Vyas <arvindv@grepruby.com>
2016-06-01 12:45:21 +0530

    Added order with weight for country and business filter

--- app/repositories/searchable_by_country_business_type_id.rb
+++ app/repositories/searchable_by_country_business_type_id.rb
@@ -3,7 +3,7 @@ module SearchableByCountryBusinessTypeId
     query = '*' if query.blank? || query.nil?
     results = klass.search(query, fields: [:name, :tag_name], where: condition,
                                   misspellings: false,
-                                  order: { feature: :desc },
+                                  order: { feature: :desc, weight: :desc },
                                   page: page, per_page: 10)
 
     [results.records, results.total_count]
