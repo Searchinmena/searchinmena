1126b3d7203f9afe975f2ae6971655c51c701231
shahidbscs <shahidbscs@gmail.com>
2016-07-10 18:15:29 +0400

    Update category page link with category name for search result

--- app/presenters/category_presenter.rb
+++ app/presenters/category_presenter.rb
@@ -1,11 +1,13 @@
 class CategoryPresenter
   takes :category
+  require 'uri'
 
   def as_json(*)
     {
       id: category.id,
       name: category.translations.first.value,
-      parent_id: category.parent_id
+      parent_id: category.parent_id,
+      path: ERB::Util.url_encode(category.translations.first.value)
     }
   end
 end
