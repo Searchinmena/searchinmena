b7f86daea3cc5bff140ad634c80e6dea884d2670
shahidbscs <shahidbscs@gmail.com>
2016-07-11 12:33:33 +0400

    Fix too long line issue

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -65,13 +65,15 @@ SitemapGenerator::Sitemap.create do
   # Product categories
   Category.where(type: 'ProductCategory').find_each do |ct|
     c = CategoryTranslation.where(category_id: ct.id, locale: 'en')
-    add "#{Sim::Routes.product_category_path}#{ERB::Util.url_encode(c[0].value)}",
+    name = ERB::Util.url_encode(c[0].value)
+    add "#{Sim::Routes.product_category_path}#{name}",
         priority: 1.0, changefreq: 'daily', lastmod: c[0].updated_at
   end
   # Service categories
   Category.where(type: 'ServiceCategory').find_each do |ct|
     c = CategoryTranslation.where(category_id: ct.id, locale: 'en')
-    add "#{Sim::Routes.service_category_path}#{ERB::Util.url_encode(c[0].value)}",
+    name = ERB::Util.url_encode(c[0].value)
+    add "#{Sim::Routes.service_category_path}#{name}",
         priority: 1.0, changefreq: 'daily', lastmod: c[0].updated_at
   end
 end
